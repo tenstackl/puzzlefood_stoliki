ec7a40f7a022a2520f07e023325fa163b905cc51
Tenstack <103316707+tenstackl@users.noreply.github.com>
2022-12-18 14:04:22 +0100

    Update stoliki.js

--- stoliki.js
+++ stoliki.js
@@ -1,1 +1,42 @@
+var myArray = [
 
+    // 2 osobowe
+    {
+    'miejsca':'2',
+    // czy wolne?
+    'wolne':'Tak',
+    },
+
+    // 4 osobowe
+    {
+    'miejsca':'4',
+    // czy wolne?
+     'wolne':'Tak',
+    },
+
+    // 6 osobowe
+    {
+    'miejsca':'6',
+    // czy wolne?
+    'wolne':'Tak',
+    },
+]
+
+buildTable(myArray)
+
+
+
+function buildTable(data){
+    var table = document.getElementById('myTable')
+
+    for (var i = 0; i < data.length; i++){
+        var row = `<tr>
+                        <td>${data[i].miejsca} osobowe</td>
+                        <td>${data[i].wolne}</td>
+                        
+                  </tr>`
+        table.innerHTML += row
+
+
+    }
+}
